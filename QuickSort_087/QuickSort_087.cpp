f73d5f9ddab12cc824aca2a5e3fc0b0fa18c3da4
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-03-31 15:06:54 +0700

    selesai membuat prosedur input

--- QuickSort_087/QuickSort_087.cpp
+++ QuickSort_087/QuickSort_087.cpp
@@ -7,4 +7,23 @@ int cmp_count = 0; // number of comparision
 int mov_count = 0; // number of data movement
 int n;
 
-void input(){}
+void input() {
+	while (true) {
+		cout << "Masukan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMasukan Panjang array adalah 20" << endl;
+	}
+
+	cout << "\n===================" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n===================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "<" << (i + i) << ">";
+		cin >> arr[i];
+	}
+}
